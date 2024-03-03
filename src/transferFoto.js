27f552cea37bc07e0f2644d741b2a7ad5dcffbc5
anfehero <anfehero@hotmail.com>
2024-03-02 21:30:41 -0300

    transferfoto funcionando, precisa de ajustes

--- src/transferFoto.js
+++ src/transferFoto.js
@@ -3,9 +3,9 @@ const path = require('path');
 const xlsx = require('xlsx');
 
 // Caminhos para a pasta de origem e destino
-const pastaOrigemImagens = 'C:/Users/timed/Documents/IMAGENS RAIZ';
-const pastaDestinoImagens = 'C:/Users/timed/Desktop/IMAGENS FINAL';
-const caminhoArquivoExcelImagens = 'C:/Users/timed/Downloads/relatorio_imagens.xlsx';
+const pastaOrigemImagens = 'C:/Users/RealMayer/Documents/VIVO';
+const pastaDestinoImagens = 'C:/Users/RealMayer/Desktop/MORTO';
+const caminhoArquivoExcelImagens = 'C:/Users/RealMayer/Documents/relatorio.xlsx';
 
 // Função para ler o arquivo Excel e obter os códigos
 function obterCodigosDoExcelImagens() {
@@ -14,35 +14,28 @@ function obterCodigosDoExcelImagens() {
   const planilha = workbook.Sheets[nomePrimeiraPlanilha];
   const celulas = xlsx.utils.sheet_to_json(planilha);
 
-  console.log('Conteúdo lido do Excel para imagens:', celulas);
-
   // Filtra os códigos válidos
   const codigosValidos = celulas
     .map(linha => {
-      const codigoImagem = `f${linha.Codigo || ''}`;
-      console.log('Código da imagem:', codigoImagem);
-
-      // Concatena "f" com o código, removendo espaços em branco e outros caracteres não alfanuméricos
-      const codigoConcatenado = codigoImagem.replace(/[^a-zA-Z0-9]/g, '');
-      console.log('Código após a transformação:', codigoConcatenado);
-
-      return codigoConcatenado;
-    })
-    .filter(codigo => /^f\d+$/i.test(codigo));
-
-  console.log('Códigos válidos para imagens:', codigosValidos);
+      const codigoImagem = `F${linha.codigo}`; // Adiciona o prefixo 'F' ao código do Excel
+      return codigoImagem;
+    });
 
   return codigosValidos;
 }
 
 // Função para verificar se um arquivo é uma imagem e tem o formato esperado
 function isImagemValida(arquivo) {
-  return /\.(jpg|jpeg|png)$/i.test(arquivo);
+  const codigosDoExcelImagens = obterCodigosDoExcelImagens();
+
+  // Extrai o código da imagem do nome do arquivo
+  const codigoDaImagem = arquivo.match(/^(f|F)\d+-\d+-\d+/);
+
+  // Verifica se o arquivo é uma imagem e se o código está nos códigos do Excel
+  return /\.(jpg|jpeg|png)$/i.test(arquivo) && codigoDaImagem && codigosDoExcelImagens.includes(codigoDaImagem[0].toUpperCase());
 }
 
-// Função para mover arquivos de imagem com base nos códigos do Excel
 function moverImagensComBaseNoExcel() {
-  const codigosDoExcelImagens = obterCodigosDoExcelImagens();
   let imagensMovidas = 0;
 
   // Lê os arquivos da pasta de origem de imagens
@@ -55,23 +48,17 @@ function moverImagensComBaseNoExcel() {
     // Filtra apenas os arquivos de imagem válidos
     const imagensValidas = arquivos.filter(isImagemValida);
 
-    console.log('Imagens válidas:', imagensValidas);
-
     // Itera sobre cada imagem válida
     imagensValidas.forEach(imagem => {
-      const codigoDaImagem = imagem.replace(/^f(\d+)\.(jpg|jpeg|png)$/i, '$1'); // Extrai o código da imagem
+      const caminhoOrigemImagem = path.join(pastaOrigemImagens, imagem);
+      const caminhoDestinoImagem = path.join(pastaDestinoImagens, imagem);
 
-      // Verifica se o código da imagem está nos códigos do Excel para imagens
-      if (codigosDoExcelImagens.includes(`f${codigoDaImagem}`)) {
-        const caminhoOrigemImagem = path.join(pastaOrigemImagens, imagem);
-        const caminhoDestinoImagem = path.join(pastaDestinoImagens, imagem);
+      // Move a imagem (recorta e cola)
+      fs.renameSync(caminhoOrigemImagem, caminhoDestinoImagem);
 
-        // Move a imagem (recorta e cola)
-        fs.renameSync(caminhoOrigemImagem, caminhoDestinoImagem);
-
-        console.log(`Imagem movida: ${imagem}`);
-        imagensMovidas++;
-      }
+      imagensMovidas++;
+      
+      console.log(`Imagem movida: ${imagem}`);
     });
 
     // Verifica quantas imagens foram movidas
@@ -83,5 +70,4 @@ function moverImagensComBaseNoExcel() {
   });
 }
 
-// Chama a função para mover as imagens com base nos códigos do Excel para imagens
 moverImagensComBaseNoExcel();
