0b5273cf2a52ef228693a6d01f9a680f219547a6
anfehero <anfehero@hotmail.com>
2024-03-02 22:18:52 -0300

    TransferFoto Done - TransferArchive almost

--- src/transferFoto.js
+++ src/transferFoto.js
@@ -2,72 +2,69 @@ const fs = require('fs');
 const path = require('path');
 const xlsx = require('xlsx');
 
-// Caminhos para a pasta de origem e destino
-const pastaOrigemImagens = 'C:/Users/RealMayer/Documents/VIVO';
-const pastaDestinoImagens = 'C:/Users/RealMayer/Desktop/MORTO';
-const caminhoArquivoExcelImagens = 'C:/Users/RealMayer/Documents/relatorio.xlsx';
+// Paths
+const sourceFolder = 'C:/Users/RealMayer/Documents/VIVO';
+const destinationFolder = 'C:/Users/RealMayer/Desktop/MORTO';
+const excelFilePath = 'C:/Users/RealMayer/Documents/relatorio.xlsx';
 
-// Função para ler o arquivo Excel e obter os códigos
-function obterCodigosDoExcelImagens() {
-  const workbook = xlsx.readFile(caminhoArquivoExcelImagens);
-  const nomePrimeiraPlanilha = workbook.SheetNames[0];
-  const planilha = workbook.Sheets[nomePrimeiraPlanilha];
-  const celulas = xlsx.utils.sheet_to_json(planilha);
+// Funcao para ler o excel e pegar os codigos
+function getCodesFromExcel() {
+  const workbook = xlsx.readFile(excelFilePath);
+  const firstSheetName = workbook.SheetNames[0];
+  const sheet = workbook.Sheets[firstSheetName];
+  const cells = xlsx.utils.sheet_to_json(sheet);
 
-  // Filtra os códigos válidos
-  const codigosValidos = celulas
-    .map(linha => {
-      const codigoImagem = `F${linha.codigo}`; // Adiciona o prefixo 'F' ao código do Excel
-      return codigoImagem;
-    });
+  // Filtra os codigos validos
+  const validCodes = cells.map(row => {
+    const codeImage = `F${row.codigo}`; // Adiciona o 'F' no prefixo do codigo do excel
+    return codeImage;
+  });
 
-  return codigosValidos;
+  return validCodes;
 }
 
-// Função para verificar se um arquivo é uma imagem e tem o formato esperado
-function isImagemValida(arquivo) {
-  const codigosDoExcelImagens = obterCodigosDoExcelImagens();
+// Função chea se o arquivos é uma imagem e se tem o formato correto
+function isImageValid(file) {
+  const excelImageCodes = getCodesFromExcel();
 
-  // Extrai o código da imagem do nome do arquivo
-  const codigoDaImagem = arquivo.match(/^(f|F)\d{3,6}-?\d{2,3}-\d{3}/);
+  // Extrai o codigo da imagem do nome do arquivo
+  const imageCode = file.match(/^(f|F)\d{3,6}-?\d{2,3}-\d{3}/);
 
-  // Verifica se o arquivo é uma imagem e se o código está nos códigos do Excel
-  return codigoDaImagem && codigosDoExcelImagens.includes(codigoDaImagem[0].toUpperCase());
+  // Checa se o arquivos é uma imagem e se o codigo esta no excel
+  return imageCode && excelImageCodes.includes(imageCode[0].toUpperCase());
 }
 
-function moverImagensComBaseNoExcel() {
-  let imagensMovidas = 0;
+function moveImagesBasedOnExcel() {
+  let movedImagesCount = 0;
 
-  // Lê os arquivos da pasta de origem de imagens
-  fs.readdir(pastaOrigemImagens, (err, arquivos) => {
+  // Lê os arquivos da pasta raiz
+  fs.readdir(sourceFolder, (err, files) => {
     if (err) {
-      console.error('Erro ao ler a pasta de origem de imagens:', err);
+      console.error('Erro ao ler a pasta RAIZ da imagem: ', err);
       return;
     }
 
-    // Filtra apenas os arquivos de imagem válidos
-    const imagensValidas = arquivos.filter(isImagemValida);
+    // Filtra apenas as imagens validas
+    const validImages = files.filter(isImageValid);
 
-    // Itera sobre cada imagem válida
-    imagensValidas.forEach(imagem => {
-      const caminhoOrigemImagem = path.join(pastaOrigemImagens, imagem);
-      const caminhoDestinoImagem = path.join(pastaDestinoImagens, imagem);
+    // Itera cada imagem valida
+    validImages.forEach(image => {
+      const sourceImagePath = path.join(sourceFolder, image);
+      const destinationImagePath = path.join(destinationFolder, image);
 
-      // Move a imagem (recorta e cola)
-      fs.renameSync(caminhoOrigemImagem, caminhoDestinoImagem);
+      // Move as imagens (recorte e cola)
+      fs.renameSync(sourceImagePath, destinationImagePath);
 
-      imagensMovidas++;
-      
-      console.log(`Imagem movida: ${imagem}`);
+      movedImagesCount++;
     });
 
-    // Verifica quantas imagens foram movidas
-    if (imagensMovidas > 0) {
-      console.log(`${imagensMovidas} imagens foram movidas com sucesso.`);
+    // Checa quantas imagens foi movida
+    if (movedImagesCount > 0) {
+      console.log(`${movedImagesCount} As imagens foram movidas com sucesso.`);
     } else {
-      console.log('Nenhuma imagem foi movida.');
+      console.log('Nenhuma imagem movida.');
     }
   });
 }
 
-moverImagensComBaseNoExcel();
+moveImagesBasedOnExcel();
